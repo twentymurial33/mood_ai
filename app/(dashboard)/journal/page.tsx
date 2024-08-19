777eee8246302378b173568c53ac4bfe8b5a4950
Murial Anindo <murial.anindo@lonett.com>
2024-08-19 15:14:30 -0400

    added journal id

--- app/(dashboard)/journal/page.tsx
+++ app/(dashboard)/journal/page.tsx
@@ -1,5 +1,5 @@
-import EntryCard from '@/components/EntryCard'
-import NewEntryCard from '@/components/NewEntryCard'
+import EntryCard from '../../../components/EntryCard'
+import NewEntryCard from '../../../components/NewEntryCard'
 import { getUserByClerkID } from '@/utils/auth'
 import { prisma } from '@/utils/db'
 
--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,15 @@
 import {auth} from '@clerk/nextjs'
 import Link from 'next/link';
 export default async function Home() {
-  const {userId}= await auth();
+  const { userId } = await auth()
+  let href = userId ? '/journal' : '/new-user'
   return (
        <div className="w-screen h-screen bg-black flex justify-center items-center text-white">
         <div className="w-full max-w-[600px] mx-auto">
           <h1 className="text-6xl mb-4">The best journal app</h1>
           <p className="text-2xl text-white/50 mb-4">This is the best app for tracking your mood in your life</p>
           <div>
-            <Link href="/journal">
+            <Link href={href}>
             <button className="bg-blue-600 px-4 py-2 rounded-lg text-xl">Get Started</button>
             </Link>
           </div>
