cfccb7f257ddf5b305d28637b36a4256385c194c
Murial Anindo <murial.anindo@lonett.com>
2024-08-20 17:44:05 -0400

    added api file to utils and question and editor files

--- app/new-user/page.tsx
+++ app/new-user/page.tsx
@@ -3,23 +3,23 @@ import { currentUser } from '@clerk/nextjs'
 import { redirect } from 'next/navigation'
 
 const createNewUser = async () => {
-  const user = await currentUser()
-  console.log(user)
+  // const user = await currentUser()
+  // console.log(user)
 
-  const match = await prisma.user.findUnique({
-    where: {
-      clerkId: user.id as string,
-    },
-  })
+  // const match = await prisma.user.findUnique({
+  //   where: {
+  //     clerkId: user.id as string,
+  //   },
+  // })
 
-  if (!match) {
-    await prisma.user.create({
-      data: {
-        clerkId: user.id,
-        email: user?.emailAddresses[0].emailAddress,
-      },
-    })
-  }
+  // if (!match) {
+  //   await prisma.user.create({
+  //     data: {
+  //       clerkId: user.id,
+  //       email: user?.emailAddresses[0].emailAddress,
+  //     },
+  //   })
+  // }
 
   redirect('/journal')
 }
